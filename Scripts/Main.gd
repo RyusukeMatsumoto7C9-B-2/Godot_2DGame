24d75f0be3c34a302cfd8dc77aeaa5e6ebfa7471
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2023-10-28 17:06:33 +0900

    BGM と SE を追加した

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -14,12 +14,16 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	get_tree().call_group("mobs", "queue_free")
+	
 	
 
 func _on_score_timer_timeout():
