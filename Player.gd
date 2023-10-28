a3b30eed5c57b806f9bf26112dc28fd3708cf9ef
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2023-10-28 15:27:00 +0900

    Player の当たり判定を追加

--- Player.gd
+++ Player.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal hit
+
 @export var speed = 400
 var screen_size
 
@@ -8,7 +10,13 @@ func _ready():
 	screen_size = get_viewport_rect().size
 
 
-# 舞いフレーム処理.
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+
+# 毎フレーム処理.
 func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
@@ -26,4 +34,7 @@ func _process(delta):
 		position = position.clamp(Vector2.ZERO, screen_size)
 
 
-signal hit
+func _on_body_entered(body):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
